6a60dbc5bed750d648b09f7ecd99f1fdb98ad4b5
Leman Fotera <lemanfoetra@gmail.com>
2020-06-19 20:50:46 +0700

    bug pixed foreach pada saat object null

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -5,14 +5,11 @@ import 'package:http/http.dart' as http;
 import './product.dart';
 
 class ProductsProvider with ChangeNotifier {
-
   final String authToken;
   List<Product> _items = [];
 
-
   // construct
   ProductsProvider(this.authToken);
-
 
   List<Product> get items {
     return [..._items];
@@ -24,22 +21,25 @@ class ProductsProvider with ChangeNotifier {
 
   // GET DATA DARI FIREBASE
   Future<void> getListProduct() async {
-    final url = "https://flutter-shopapps.firebaseio.com/product.json?auth=$authToken";
+    final url =
+        "https://flutter-shopapps.firebaseio.com/product.json?auth=$authToken";
 
     try {
       final response = await http.get(url);
       final ekstrakData = json.decode(response.body) as Map<String, dynamic>;
       List<Product> newData = [];
-      ekstrakData.forEach((key, value) {
-        newData.add(Product(
-          id: key,
-          description: value['description'],
-          imageUrl: value['imageUrl'],
-          price: value['price'],
-          title: value['title'],
-          isFavorite: value['isFavorite'],
-        ));
-      });
+      if (ekstrakData != null) {
+        ekstrakData.forEach((key, value) {
+          newData.add(Product(
+            id: key,
+            description: value['description'],
+            imageUrl: value['imageUrl'],
+            price: value['price'],
+            title: value['title'],
+            isFavorite: value['isFavorite'],
+          ));
+        });
+      }
       _items = newData;
       notifyListeners();
     } catch (error) {
